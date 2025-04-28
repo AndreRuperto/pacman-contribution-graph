b57daa65e328f6746059864d5f0922aaae462251
André Ruperto <andreruperto@gmail.com>
2025-04-28 10:30:44 -0300

    Remove pastas dist e scripts do repositório

--- dist/logger.js
+++
@@ -1,9 +0,0 @@
-import { appendFileSync } from 'fs';
-const getTimestamp = () => {
-    return new Date().toISOString().replace('T', ' ').split('.')[0];
-};
-export const logToFile = (message) => {
-    const timestamp = getTimestamp();
-    const fullMessage = `[${timestamp}] ${message}\n`;
-    appendFileSync('log_pacman.txt', fullMessage, 'utf-8');
-};
