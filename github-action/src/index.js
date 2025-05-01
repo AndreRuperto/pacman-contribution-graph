53704e816ab07bd7c8ea712b1805538a3a0b2f2f
André Ruperto <andreruperto@gmail.com>
2025-05-01 08:59:14 -0300

    fix: update index.ts and publish new version to NPM

--- github-action/src/index.js
+++ github-action/src/index.js
@@ -1,7 +1,7 @@
 import * as core from '@actions/core';
 import * as fs from 'fs';
 import * as path from 'path';
-import { PacmanRenderer } from '../../dist/pacman-contribution-graph.min.js';
+import { PacmanRenderer } from 'svg-pacman-contributions';
 
 async function generateSvg(username, token, theme) {
   return new Promise((resolve) => {
